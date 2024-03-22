5f285d6a97733cc7795240db9f883ec78b00f944
Pankaj Poudel <poudelpankaj9@gmail.com>
2024-03-22 21:14:50 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Figure from './components/Figure';
+import Header from './components/Header';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import { showNotification as show } from './helpers/helper';
+import Notifications from './components/Notifications';
+import PopUp from './components/PopUp';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+
+
+// App Component and State Management
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+  // const [selectedWord, setSelectedWord] = useState(words[Math.floor(Math.random() * words.length)]);
+
+  //useEffect Hook for Keyboard Events
+  useEffect(() => {
+    const handleKeydown = event => {  //It extracts the key and keyCode properties from the event object using destructuring assignment.
+    const { key, keyCode } = event; // The purpose of this function is to handle the logic associated with a key press.
+
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {  // Checks if the selected word contains the pressed letter.
+          if (!correctLetters.includes(letter)) {  //Checks if the correctLetters state array does not already include the pressed letter.
+            setCorrectLetters(currentLetters => [...currentLetters, letter]); // If both conditions are true, it updates the state by adding the pressed letter to the correctLetters array.
+
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) { //Checks if the wrongLetters state array does not already include the pressed letter
+            setWrongLetters(wrongLetters => [...wrongLetters, letter]); //If both conditions are true, it updates the state by adding the pressed letter to the wrongLetters array.
+
+
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+
+    };
+    window.addEventListener('keydown', handleKeydown);
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable, /*selectedWord*/ ]);
+  function playAgain() {
+    setPlayable(true);
+
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
+  // Return JSX for Rendering
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+       
+      </div>
+      <Notifications showNotification={showNotification}/>
+        <PopUp correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain}  />
     </div>
   );
 }
 
+//Exporting the Component
 export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <h1>
+                Hangman
+            </h1>
+            <p>find the hidden word -- enter  the letter</p>
+
+        </>
+    );
+}
+
+export default Header
